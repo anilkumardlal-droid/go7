12ef437b88396aed642c3b1897b0a8fc0c692019
anilkumardlal-droid <anilkumardlal@gmail.com>
2026-07-31 10:02:14 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,11 @@
 (function () {
 
-    function removeSkeleton() {
-        const content = document.getElementById("go7Content");
+    const content = document.getElementById("go7Content");
 
-        if (content) {
-            content.classList.remove("go7-content-loading");
-        }
-    }
+    if (!content) return;
 
-    // Skeleton maximum 4.5 seconds
-    setTimeout(removeSkeleton, 4500);
+    setTimeout(function () {
+        content.classList.remove("go7-content-loading");
+    }, 4500);
 
 })();
