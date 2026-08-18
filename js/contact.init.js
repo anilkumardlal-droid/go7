f5a8ad1ea9cae61921dda29dfb3014ba519b015a
anilkumardlal-droid <anilkumardlal@gmail.com>
2026-08-19 02:24:40 +0530

    Update contact.init.js

--- js/contact.init.js
+++ js/contact.init.js
@@ -151,7 +151,7 @@ const s = String(remaining % 60).padStart(2, "0");
 feedback.innerHTML = `
 <div style="line-height:1.7">
 
-<strong style="display:block;color:#B45309;font-size:16px;font-weight:400;">
+<strong style="display:block;color:#B45309;font-size:15px;font-weight:600;">
 Access has been temporarily restricted after 3 requests.
 </strong>
 
