e156f07f0754f27be1142553bd99a8cd652f9f47
anilkumardlal-droid <anilkumardlal@gmail.com>
2026-09-15 12:20:30 +0530

    Create tawk.js

---
+++ js/tawk.js
@@ -0,0 +1,14 @@
+var Tawk_API = Tawk_API || {},
+    Tawk_LoadStart = new Date();
+
+(function () {
+    var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+
+    s1.async = true;
+    s1.src = "https://embed.tawk.to/6aa8e6319117af34473db58d/1k2hs6cod";
+    s1.charset = "UTF-8";
+    s1.setAttribute("crossorigin", "*");
+
+    s0.parentNode.insertBefore(s1, s0);
+})();
